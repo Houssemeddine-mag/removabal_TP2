8947fecfc7cff5bfb4be39f9a137f488ce3c1417
Houssemeddine-mag <houssem.magra@univ-constantine2.dz>
2025-11-19 11:15:56 +0100

    d

--- spring/src/main/java/tpspring/spring/model/University.java
+++ spring/src/main/java/tpspring/spring/model/University.java
@@ -1,30 +1,61 @@
 package tpspring.spring.model;
+
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "universities")
 public class University {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
+
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false)
+    private String location;
+
+    // Constructors
     public University() {
     }
 
-    public int getId() {
+    public University(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    // Getters and Setters
+    public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public void setName(String name) {
         this.name = name;
     }
-    
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return "University{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                '}';
+    }
 }
